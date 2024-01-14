fdbec23514d45387defe0540a20064996a83d516
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-01-13 20:40:10 -0700

    Refactor Program.cs: Update console output to prompt for grade percentage input, then display lettergrade and pass or fail.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePerc = int.Parse(grade);
+        string letterGrade;
+        string sign;
+        if (gradePerc >= 90) 
+        {
+            letterGrade = "A";
+        }
+        else if (gradePerc >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePerc >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePerc >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (gradePerc % 10 >= 7 && letterGrade != "A" && letterGrade != "F")
+        {
+            sign = "+";
+        }
+        else if (gradePerc %  10 <= 3 && letterGrade != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"You got a/an {letterGrade}{sign}");
+
+        if (gradePerc >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Next time you'll do great!");
+        }
     }
 }

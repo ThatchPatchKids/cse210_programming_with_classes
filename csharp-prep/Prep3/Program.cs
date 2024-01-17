df1fc21e093327f9b47adb39aca71637822593da
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-01-16 17:50:05 -0700

    Update Program.cs in Prep3: Added a number guessing game for a random number between 1 and 100.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do 
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+            bool found = false;
+            int guessNumber;
+            int guesses = 0;
+            while (!found)
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                guesses += 1;
+
+                if (guessNumber == magicNumber)
+                {
+                    found = true;
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            Console.WriteLine($"Congratulations! You guessed the number {magicNumber}!");
+            Console.WriteLine($"You found it in {guesses} guesses.");
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+            
+        } while (response == "yes");
+
+
     }
 }

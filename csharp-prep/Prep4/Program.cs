d95c6e83c02fe916390933ab19bf47dff68938f5
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-01-17 12:38:55 -0700

    Updated Program.cs in Prep4: Added code that asks the user for a series of numbers, appends each one to a list, finds the sum of the list, the average, the largest number, the smallest positive number, sorts and displays the list to the user.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do {
+            Console.Write("Enter a number: ");
+            string numString = Console.ReadLine();
+            number = int.Parse(numString);
+            numbers.Add(number);
+
+        } while (number != 0);
+
+        int sum = numbers.Sum();
+        int avg = sum / numbers.Count;
+        int largestNum = numbers.Max();
+        var positiveNumbers = numbers.Where(num => num > 0);
+        int smallestPositive = positiveNumbers.Min();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        // numbers.ForEach(number => Console.WriteLine(number));
+        numbers.Sort(); numbers.ForEach(Console.WriteLine);
     }
 }

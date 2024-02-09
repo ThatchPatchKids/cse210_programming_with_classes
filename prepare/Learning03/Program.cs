b95772f0f066d64a893f000f45c9a41a5ad95bdc
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-02-08 20:53:28 -0700

    Add: Added a Fraction class to the Learning03 folder. Wrote a program that creates different fraction objects and displays them as fractions and decimals.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {   
+        // 
+        Fraction fraction = new Fraction();
+        // 
+        Console.WriteLine(fraction.GetFractionString());
+        // 
+        Console.WriteLine(fraction.GetDecimalValue());
+
+        //
+        Fraction myFraction = new Fraction(5);
+        // 
+        Console.WriteLine(myFraction.GetFractionString());
+        //
+        Console.WriteLine(myFraction.GetDecimalValue());
+
+        //
+        Fraction yourFraction = new Fraction(3, 4);
+        //
+        Console.WriteLine(yourFraction.GetFractionString());
+        //
+        Console.WriteLine(yourFraction.GetDecimalValue());
+
+        Fraction theirFraction = new Fraction(1, 3);
+        //
+        Console.WriteLine(theirFraction.GetFractionString());
+        //
+        Console.WriteLine(theirFraction.GetDecimalValue());
+
     }
 }

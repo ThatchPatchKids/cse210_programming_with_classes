6d7f711e3b716dbdd780b61f7a7828d8c121ea9f
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-04-02 20:28:04 -0600

    Completed learning activity 5: wrote a program that computes the areas of different shapes.

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,18 @@
+
+
+using System.Diagnostics.CodeAnalysis;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        this._radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(this._radius, 2);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("yellow", 5);
+        Circle circle = new Circle("red", 4);
+        Rectangle rectangle = new Rectangle("blue", 5, 2);
+
+        List<Shape> shapes = new List<Shape>()
+        {
+            square,
+            circle,
+            rectangle
+        };
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"The shapes color is: {shape.GetColor()} and its area is: {shape.GetArea()}");
+        }
     }
 }

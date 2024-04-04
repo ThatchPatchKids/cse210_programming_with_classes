58f50bb1bacf360875da4897c24421d869ad2dea
ThatchPatchKids <thatcheralbiston@gmail.com>
2024-04-03 22:57:51 -0600

    Fix: Changed constants to floating points by adding .0, so that calculations were correct.

--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -12,19 +12,19 @@ public class Swimming : Activity
 
     public override double GetDistance()
     {
-        Console.WriteLine($"Laps: {this._laps} Distance: {this._laps * 50 / 1000}");
-        return this._laps * 50 / 1000;
+        // Console.WriteLine($"Laps: {this._laps} Distance: {this._laps * 50 / 1000}");
+        return this._laps * 50.0 / 1000.0;
     }
 
     public override double GetSpeed()
     {
-        Console.WriteLine($"Distance: {this.GetDistance()} Minutes: {base.GetLength()} Distance / Minutes: {this.GetDistance() / base.GetLength()} Distance / Minutes x 60: {(this.GetDistance() / base.GetLength()) * 60}");
+        // Console.WriteLine($"Distance: {this.GetDistance()} Minutes: {base.GetLength()} Distance / Minutes: {this.GetDistance() / base.GetLength()} Distance / Minutes x 60: {(this.GetDistance() / base.GetLength()) * 60}");
         return (this.GetDistance() / base.GetLength()) * 60;
     }
 
     public override double GetPace()
     {
-        Console.WriteLine($"Minutes: {base.GetLength()} Distance: {this.GetDistance()} Minutes / Distance: {base.GetLength() / this.GetDistance()}");
+        // Console.WriteLine($"Minutes: {base.GetLength()} Distance: {this.GetDistance()} Minutes / Distance: {base.GetLength() / this.GetDistance()}");
         return base.GetLength() / this.GetDistance();
     }
 }
